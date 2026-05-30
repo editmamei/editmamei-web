15fc0e1a755b0379daa7a087d60b722f36f779f0
abeall41 <abeall41@terpmail.umd.edu>
2026-05-30 08:28:30 -0400

    Wire in 32px PNG favicon, drop scaffold SVG

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import SiteHeader from '$lib/components/SiteHeader.svelte';
 	import SiteFooter from '$lib/components/SiteFooter.svelte';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32.png" />
+</svelte:head>
 
 <div class="flex min-h-screen flex-col bg-white text-neutral-900 antialiased">
 	<SiteHeader />
